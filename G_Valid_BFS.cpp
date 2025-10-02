7ec98cefc287eb4245c4669fcf8a33711e10f8d8
SiAn20 <sidneyangellysg@gmail.com>
2025-10-01 22:33:56 -0400

    feat(graphs): implement BFS validation logic in G_Valid_BFS.cpp

--- G_Valid_BFS.cpp
+++ G_Valid_BFS.cpp
@@ -7,7 +7,31 @@ using namespace std;
 // #define int long long
 #define endl '\n'
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> cam(n);
+  vector<vector<int>> gr(n);
+
+  for (int i = 0; i < (n - 1); ++i) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    gr[a].push_back(b);
+    gr[b].push_back(a);
+  }
+  for (int &x : cam)
+    cin >> x;
+
+  for (int i = 0; i < (n - 2); ++i) {
+    if (find(gr[cam[i]].begin(), gr[cam[i]].end(), cam[i + 1]) == gr[cam[i]].end()) {
+      cout << "No" << endl;
+      return;
+    }
+  }
+  cout << "Yes" << endl;
+}
 
 signed main() {
   ios::sync_with_stdio(false);
